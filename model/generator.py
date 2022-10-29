f076aeec9b3b2d6a66b384158cf293a437b96c96
Ryo-nakao <ryonakao0329@gmail.com>
2022-10-29 20:06:17 +0900

    input speaker emb

--- model/generator.py
+++ model/generator.py
@@ -6,7 +6,7 @@ from res_stack import ResStack
 
 
 class Generator(nn.Module):
-    """Encoder module:
+    """Generator module:
     """
     def __init__(self):
         super(Generator, self).__init__()
@@ -20,6 +20,8 @@ class Generator(nn.Module):
         )
             
         upsamples = []
+        emb_1_1=[]
+        res_stacks = []
         for i in range(4):
             up_layer = nn.Sequential(
                 nn.utils.weight_norm(nn.ConvTranspose1d(
@@ -29,12 +31,25 @@ class Generator(nn.Module):
                     stride=8 if i in [0,1] else 2,
                     padding=4 if i in [0,1] else 1,
                 )),
-                ResStack(
+                nn.LeakyReLU(0.2),
+            )
+            
+            conv_1_1 = nn.utils.weight_norm(nn.Conv1d(
+            in_channels=128,
+            out_channels=int(512/(2**(i+1))),
+            kernel_size=1,
+            ))
+            
+            residual_stack = ResStack(
                     int(512/(2**(i+1))),
                 )
-            )
+            
             upsamples.append(up_layer)
+            emb_1_1.append(conv_1_1)
+            res_stacks.append(residual_stack)
         self.upsamples = nn.ModuleList(upsamples)
+        self.emb_1_1 = nn.ModuleList(emb_1_1)
+        self.res_stacks = nn.ModuleList(res_stacks)
 
         self.conv_3 = nn.utils.weight_norm(nn.Conv1d(
             in_channels=32,
@@ -44,14 +59,17 @@ class Generator(nn.Module):
             padding=3,
             ))
 
-    def forward(self, x):
+    def forward(self, x, emb_org):
         x = self.conv_1(x)
         x = F.relu(x)
         x = self.conv_2(x)
         x = F.relu(x)
 
-        for upsample in self.upsamples:
-            x = F.gelu(upsample(x))
+        for upsample, emb11, res_stack in zip(self.upsamples, self.emb_1_1, self.res_stacks):
+            x = upsample(x)
+            emb = F.relu(emb11(emb_org))
+            
+            x = F.relu(res_stack(x+emb))
 
         x = self.conv_3(x)
         x = torch.tanh(x)
